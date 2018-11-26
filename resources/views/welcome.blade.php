bdf3997f4b43642df0f368ace10c0352f8888471
GiovannoPratama <pratamagiovanno@gmail.com>
2018-11-26 16:13:29 +0700

    dasbord welocom.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,20 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href='//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.css' rel='stylesheet'/>
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background: 'wallpaperpostingan.jpg';
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
@@ -65,7 +70,7 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height" style="background-color: gold">
+        <div class="flex-center position-ref full-height" style="margin-left: 300px;">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
@@ -78,17 +83,78 @@
             @endif
 
             <div class="content">
+                <p>Ojo bingun lan ojo gupuh, Seng Penting Yakin</p>
+                <p>Selamat datang, selamat berbelanja</p>
                 <div class="title m-b-md">
                     Hajatku
                 </div> 
-
-                <p>Ojo bingun lan ojo gupuh, Seng Penting Yakin</p>
-
-                <a href="{{url('member/register')}}" class="btn btn-primary"><h3>Daftar</a>
-                <a href="{{url('member')}}" class="btn btn-primary">Login</h3></a>
-
-               
             </div>
+            <div class="container" style="margin-left: 10px;">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading"><center>Login</center></div>
+
+                        <div class="panel-body">
+                            <form class="form-horizontal" method="POST" action="{{ route('member.login') }}">
+                                {{ csrf_field() }}
+
+                                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                    <label for="email" class="col-md-4 control-label"><p class="fa fa-user"></p></label>
+
+                                    <div class="col-md-6">
+                                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+
+                                        @if ($errors->has('email'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                    <label for="password" class="col-md-4 control-label"><p class="fa fa-lock "></p></label>
+
+                                    <div class="col-md-6">
+                                        <input id="password" type="password" class="form-control" name="password" required>
+
+                                        @if ($errors->has('password'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="col-md-6 col-md-offset-4">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="col-md-8 col-md-offset-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            Login
+                                        </button>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="col-md-8 col-md-offset-4">
+                                           Belum punya akun? <a href="{{ route('member.register') }}">Register</a>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
     </body>
 </html>
