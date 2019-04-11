2e018d7635a2552410001bfe3995c362db381ed0
syaifudin <syaifudin@dikanetwork.com>
2019-04-11 15:01:35 +0700

    ar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+        {{-- init ker --}}
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <!-- Styles -->
